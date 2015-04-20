759bf1a00853cae0f2971796c6562481db972c37
 Rofiq <rofiq@Detiks-MacBook-Air-2.local>
2015-04-20 18:46:52 +0700

    add

--- src/main/scala/wayang/models/RequestBid.scala
+++ src/main/scala/wayang/models/RequestBid.scala
@@ -3,6 +3,196 @@ package wayang.models
 /**
  * Created by rofiq on 4/16/15.
  */
+case class Content(
+                  id:Option[String],
+                  episode:Option[Int],
+                  title:Option[String],
+                  series:Option[String],
+                  season:Option[String],
+                  producer:Option[Producer],
+                  url:Option[String],
+                  cat:Option[List[String]],
+                  videoquality:Option[Int],
+                  context:Option[Int],
+                  contentrating:Option[String],
+                  userrating:Option[String],
+                  qagmediarating:Option[Int],
+                  keywords:Option[String],
+                  livestream:Option[Int],
+                  sourcerelationship:Option[Int],
+                  len:Option[Int],
+                  language:Option[String],
+                  embeddable:Option[Int],
+                  ext:Option[Ext]
+                    )
+
+case class Device (
+                  ua:Option[String],
+                  geo:Option[Geo],
+                  dnt:Option[Int],
+                  lmt:Option[Int],
+                  ip:Option[String],
+                  ipv6:Option[String],
+                  devicetype:Option[Int],
+                  make:Option[String],
+                  model:Option[String],
+                  os:Option[String],
+                  osv:Option[String],
+                  hwv:Option[String],
+                  h:Option[Int],
+                  w:Option[Int],
+                  ppi:Option[Int],
+                  pxratio:Option[Float],
+                  js:Option[Int],
+                  flashver:Option[String],
+                  language:Option[String],
+                  carrier:Option[String],
+                  connectiontype:Option[Int],
+                  ifa:Option[String],
+                  didsha1:Option[String],
+                  didmd5:Option[String],
+                  dpidsha1:Option[String],
+                  dpidmd5:Option[String],
+                  macsha1:Option[String],
+                  madmd5:Option[String],
+                  ext:Option[Ext]
+                    )
+
+case class Geo(
+              lat:Option[Float],
+              lon:Option[Float],
+              _type:Option[Int],
+country:Option[String],
+              region:Option[String],
+              regionfips104:Option[String],
+              metro:Option[String],
+              city:Option[String],
+              zip:Option[String],
+              utcoffset:Option[Int],
+ext:Option[Ext]
+                )
+
+case class User(
+               id:Option[String],
+               buyerid:Option[String],
+               yob:Option[Int],
+               gender:Option[String],
+               keywords:Option[String],
+               customdata:Option[String],
+               geo:Option[Geo],
+               data:Option[List[Data]],
+               ext:Option[Ext]
+                 )
+case class Segment(
+                  id:Option[String],
+                  name:Option[String],
+                  value:Option[String],
+                  ext:Option[Ext]
+                    )
+case class Regs(
+               coppa:Option[Int],
+               ext:Option[Ext]
+                 )
+case class Pmp(
+                private_auction:Option[Int],
+              deals:Option[List[Deal]],
+              ext:Option[Ext]
+                )
+case class Deal(
+               id:String,
+               bidfloor:Float = 0.0,
+               bidfloorcur:String = "USD",
+               at:Option[Int],
+               wseat:Option[List[String]],
+               wadomain:Option[]
+                 )
+case class Data(
+               id:Option[String],
+               name:Option[String],
+               segment:Option[List[Segment]],
+               ext:Option[Ext]
+                 )
+
+case class Producer(
+                      id:Option[String],
+                      name:Option[String],
+                      cat:Option[List[String]],
+                      domain:Option[String],
+                      ext:Option[Ext]
+                      )
+
+case class Publisher(
+                    id:Option[String],
+                    name:Option[String],
+                    cat:Option[List[String]],
+                    domain:Option[String],
+                    ext:Option[Ext]
+                      )
+case class App(
+              id:Option[String],
+              name:Option[String],
+              bundle:Option[String],
+              domain:Option[String],
+              storeurl:Option[String],
+              cat:Option[List[String]],
+              sectioncat:Option[List[String]],
+              pagecat:Option[List[String]],
+              ver:Option[String],
+              privacypolicy:Option[Int],
+              paid:Option[Int],
+              publisher:Option[Publisher],
+              content:Option[Content],
+              keywords:Option[String],
+              ext:Option[Ext]
+                )
+case class Site(
+               id:Option[String],
+               name:Option[String],
+               domain:Option[String],
+               cat:Option[List[String]],
+               sectioncat:Option[List[String]],
+               pagecat:Option[List[String]],
+               page:Option[String],
+               ref:Option[String],
+               search:Option[String],
+               mobile:Option[Int],
+               privacypolicy:Option[Int],
+               publisher:Option[Publisher],
+               content:Option[Content],
+               keywords:Option[String],
+               ext:Option[Ext]
+                 )
+case class Native(
+                 request:String,
+                 ver:Option[String],
+                 api:Option[List[Int]],
+                 battr:Option[List[Int]],
+                 ext:Option[Ext]
+                   )
+case class Video(
+                mimes:List[String],
+                minduration:Option[Int],
+                maxduration:Option[Int],
+                protocol:Option[Int],
+                protocols:Option[List[Int]],
+                w:Option[Int],
+                h:Option[Int],
+                startdelay:Option[Int],
+                linearity:Option[Int],
+                sequence:Option[Int],
+                battr:Option[List[Int]],
+                maxextended:Option[Int],
+                minbitrate:Option[Int],
+                maxbitrate:Option[Int],
+                boxingallowed:Int = 1,
+                playbackmethod:Option[List[Int]],
+                delivery:Option[List[Int]],
+                pos:Option[Int],
+                companionad:Option[List[Banner]],
+                api:Option[List[Int]],
+                companiontype:Option[List[Int]],
+                ext:Option[Ext]
+                  )
 case class Banner(
                  w:Int,
                  h:Int,
@@ -26,7 +216,7 @@ case class Imp(id:String,
                 native:Option[Native],
                 displaymanager:Option[String],
                displaymanagerver:Option[String],
-                instl:Int = 0,
+ nt               instl:Int = 0,
                 tagid:String,
                 bidfloor:Float = 0,
                 bidfloorcur:String = "USD",
