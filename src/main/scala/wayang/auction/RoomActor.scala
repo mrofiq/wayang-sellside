30f92ac2be989eda6422d0b2d7ea7252130b8ab1
rofiq <mrofiq@gmail.com>
2015-04-22 07:54:04 +0700

    change to bid

--- src/main/scala/wayang/auction/RoomActor.scala
+++ src/main/scala/wayang/auction/RoomActor.scala
@@ -4,16 +4,17 @@ import akka.actor.Actor
 import wayang.models.{Bid, BidRequest}
 
 import scala.collection.SortedSet
+import scala.collection.mutable._
 
 /**
  * Created by rofiq on 4/20/15.
  */
 class RoomActor extends Actor {
-  val bids = SortedSet()
+  val bids:Buffer[Bid] = Buffer.empty
 
   override def receive = {
     case bid:Bid =>
-
+      bids += bid
       sender() ! "hello"
     case "timeout" =>
       // pick winner
