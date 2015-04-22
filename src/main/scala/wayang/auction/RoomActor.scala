881d2d4ead0b963a4759219ee8915376f123a933
 Rofiq <rofiq@Detiks-MacBook-Air-2.local>
2015-04-22 18:50:42 +0700

    update actor

--- src/main/scala/wayang/auction/RoomActor.scala
+++ src/main/scala/wayang/auction/RoomActor.scala
@@ -1,22 +1,27 @@
 package wayang.auction
 
 import akka.actor.Actor
-import wayang.models.{Bid, BidRequest}
+import wayang.models.{Imp, Bid, BidRequest}
 
 import scala.collection.SortedSet
 import scala.collection.mutable._
-
+import scala.concurrent.duration._
 /**
  * Created by rofiq on 4/20/15.
  */
-class RoomActor(request:BidRequest) extends Actor {
+case class Reject(bid:Bid)
+
+class RoomActor(imp:Imp) extends Actor {
   val bids:Buffer[Bid] = Buffer.empty
+  context.system.scheduler.scheduleOnce(200 millis, self, "timeout")
 
   override def receive = {
     case bid:Bid =>
-      bids += bid
-      sender() ! "hello"
+      if (bid.price >= imp.bidfloor)
+        bids += bid
+      else
+        sender() ! Reject(bid)
     case "timeout" =>
-      // pick winner
+      // order bid
   }
 }
